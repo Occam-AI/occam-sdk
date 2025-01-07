b7793ff74006da39b2bdaaf1c0d04aceca91dfbe
Mohamed Abdelbary <mo@occami.ai>
2025-01-07 20:41:55 +0000

    add prints and update gitignore

--- src/scripts/example_run_agent.py
+++ src/scripts/example_run_agent.py
@@ -1,13 +1,19 @@
 
+import os
 from occam_sdk.api_client import OccamClient
 
 
 if __name__ == "__main__":
+    api_key = os.getenv("OCCAM_API_KEY")
+    base_url = os.getenv("OCCAM_BASE_URL", "http://127.0.0.1:8000")
     # In your application or tests:
-    client = OccamClient(api_key="sk-occam-d75ed2b8153b4a3ba1c1a08012899551", base_url="http://127.0.0.1:8000")
+    client = OccamClient(api_key=api_key, base_url=base_url)
 
     # List agents
     agents_list = client.agents.list_agents()
+    for agent in agents_list:
+        print(f"Agent: {agent}")
+        print("======")
 
     # Get a specific agent
     some_agent = client.agents.get_agent(agent_name="WebBrowsingLlmTool")
@@ -20,6 +26,7 @@ if __name__ == "__main__":
             # ...
         }
     )
+    print(f"Created agent: {created_agent}")
 
     # Run the agent
     agent_run = client.agents.run_agent(
@@ -38,6 +45,7 @@ if __name__ == "__main__":
 
     # Check status
     run_status = client.agents.get_agent_run_status(agent_run["agent_run_instance_id"])
+    # Get run result
     run_result = client.agents.get_agent_run_result(agent_run["agent_run_instance_id"])
     print(run_status)
     print(run_result)
